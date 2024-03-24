881c885ee7c6f0312891437b914386e01a542819
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-03-24 01:55:11 -0400

    finish variable types

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Testing Section
 
+// Object 
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
 
+console.log(person.fullName());
 
-
-
-// Variable Types Section
-
-    // Number
 
 
 });
