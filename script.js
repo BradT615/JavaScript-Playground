8ed23e0573807c637e60852827c11dc3bf896af3
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-03-24 22:43:55 -0400

    update header

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ let person = {
 console.log(person.fullName());
 
 
+const header = document.getElementById('header');
+const scrolledClasses = ['w-full', 'shadow-2xl', 'm-0', 'rounded-none', 'bg-opacity-60'];
+const defaultClasses = ['w-11/12', 'shadow-md'];
 
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 20) {
+    defaultClasses.forEach(className => header.classList.remove(className));
+    scrolledClasses.forEach(className => header.classList.add(className));
+  } else {
+    scrolledClasses.forEach(className => header.classList.remove(className));
+    defaultClasses.forEach(className => header.classList.add(className));
+  }
+});
 
 });
