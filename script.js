1c7fcdb923631b215381094d23379b7f261cee39
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-03-24 22:52:38 -0400

    fix header

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let person = {
 
 console.log(person.fullName());
 
-
+// Animate Header on Scroll
 const header = document.getElementById('header');
 const scrolledClasses = ['w-full', 'shadow-2xl', 'm-0', 'rounded-none', 'bg-opacity-60'];
-const defaultClasses = ['w-11/12', 'shadow-md'];
+const defaultClasses = ['w-11/12', 'shadow-md', 'm-4', 'rounded-xl', 'bg-opacity-90'];
 
 window.addEventListener('scroll', function() {
-  if (window.scrollY > 20) {
-    defaultClasses.forEach(className => header.classList.remove(className));
-    scrolledClasses.forEach(className => header.classList.add(className));
-  } else {
-    scrolledClasses.forEach(className => header.classList.remove(className));
-    defaultClasses.forEach(className => header.classList.add(className));
-  }
+    if (window.scrollY > 20) {
+        defaultClasses.forEach(className => header.classList.remove(className));
+        scrolledClasses.forEach(className => header.classList.add(className));
+    } else {
+        scrolledClasses.forEach(className => header.classList.remove(className));
+        defaultClasses.forEach(className => header.classList.add(className));
+    }
 });
 
 });
